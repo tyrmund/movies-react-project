c58e6deca1b8f964e281dfbbb94420c906484fad
Rodolfo Bittkau <tyrmund@gmail.com>
2024-05-09 17:41:42 +0200

    connected app.jsx to database

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
+import { useState } from 'react'
+
+const API_URL = "http://localhost:5000"
 
 function App() {
 
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    loadAllMovies()
+  }, [])
+
+  const loadAllMovies = () => {
+    axios
+      .get(`${API_URL}/movies`)
+      .then(({ data }) => setMovies(data))
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className='App'>
+      {
+        movies.map((movie) => <h1>{movie.title}</h1>)
+      }
     </div>
   )
 }
